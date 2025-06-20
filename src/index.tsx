913bd5d1be8c1bf971bd5ad35a66d5f6161472ac
Fanny Bouffier <fanny.bouffier@limengo.com>
2025-06-21 00:25:14 +0200

    feat(init): added react app with chakra ui theming

---
+++ src/index.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { ChakraProvider, defaultSystem } from "@chakra-ui/react";
+import App from "./App";
+import { system } from "./theme";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    {/* Utilise le thème personnalisé via `value` */}
+    <ChakraProvider value={system}>
+      <App />
+    </ChakraProvider>
+  </React.StrictMode>,
+);
